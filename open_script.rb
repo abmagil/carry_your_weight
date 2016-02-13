00aeedc2cacd014ad923049a0484bb52151af801
Aaron Magil <aaron@societyofgrownups.com>
2016-02-13 09:58:24 -0500

    Initial, incomplete script to scrape git repo into DB.  No persistence happens yet

---
+++ open_script.rb
@@ -0,0 +1,42 @@
+require 'rugged'
+
+IGNORE_FILES = ['.gitignore', 'Gemfile.lock', '.project', 'LICENSE']
+IGNORE_DIRS = ['bin/', 'pkg/']
+
+repo = Rugged::Repository.new(".")
+
+# Get the HEAD commit from master
+master_head = repo.branches.find{|br| br.name == "master"}.target
+
+# Walk every commit on master, starting at the last one
+Rugged::Walker.walk(repo, show: master_head, sort: Rugged::SORT_DATE) do |commit|
+  puts "commit: #{commit.oid}"
+  # Count the files
+  commit.tree.walk_blobs(:postorder) do |root, entry|
+    next if IGNORE_DIRS.include? root
+    next if IGNORE_FILES.include? entry[:name]
+
+    file_path = root + entry[:name]
+    puts "\tfile: #{file_path}"
+
+    # having been handed a file
+    blame = Rugged::Blame.new(repo, file_path, newest_commit: commit)
+
+    # {"julian" => hunk}
+    author_hunks = blame.group_by{|hunk| hunk[:final_signature][:name]}
+
+    # Another reduce can be used here
+    # Reduce down all lines from this file for this author
+    author_to_lines = {}
+    author_hunks.each do |author, hunks|
+      puts "author: #{author}"
+      author_to_lines[author] = hunks.reduce(0) do |memo, hunk|
+        puts "hunk: #{hunk}"
+        memo += hunk[:lines_in_hunk]
+      end
+    puts "author_hunks: #{author_hunks}"
+    end
+  end
+
+end
+
