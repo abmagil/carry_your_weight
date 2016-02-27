d46ab9fff7bd64f9b93db092a831e09761a90791
Aaron Magil <aaron@societyofgrownups.com>
2016-02-27 13:27:07 -0500

    Use sinatrarb EventMachine recipe for stability.  Pretty far from vanilla sinatra, but nothing dies unexpectedly

--- server.rb
+++ server.rb
@@ -1,37 +1,74 @@
-
-# app.rb
-# require 'thin'
-require 'sinatra/base'
+require 'eventmachine'
 require 'em-websocket'
-require './read_script'
+require 'sinatra/base'
+require 'thin'
 
+
+# This example shows you how to embed Sinatra into your EventMachine
+# application. This is very useful if you're application needs some
+# sort of API interface and you don't want to use EM's provided
+# web-server.
+
+def run(opts)
+
+  # Start he reactor
+  EM.run do
+
+    # define some defaults for our app
+    server  = opts[:server] || 'thin'
+    host    = opts[:host]   || '0.0.0.0'
+    port    = opts[:port]   || '4567'
+    web_app = opts[:app]
+
+    dispatch = Rack::Builder.app do
+      map '/' do
+        run web_app
+      end
+    end
+
+    # Start the web server. Note that you are free to run other tasks
+    # within your EM instance.
+    Rack::Server.start({
+      app:    dispatch,
+      server: server,
+      Host:   host,
+      Port:   port,
+      signals: false,
+    })
+
+    EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
+      ws.onopen do |handshake|
+        ws.send "Connected to #{handshake.path}."
+      end
+
+      ws.onclose do
+        ws.send "Closed."
+        EM.stop
+      end
+
+      ws.onmessage do |msg|
+        puts "msg: #{msg}"
+        ws.send "Received Message: #{msg}"
+      end
+    end
+  end
+end
+
+# Our simple hello-world app
 module CarryYourWeight
   class Server < Sinatra::Base
+    # threaded - False: Will take requests on the reactor thread
+    #            True:  Will queue request for background thread
+    configure do
+      set :threaded, false
+    end
+
+    # Request runs on the reactor thread (with threaded set to false)
     get '/' do
       erb :index
     end
-
   end
 end
 
-EventMachine.run do
-
-  EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
-    ws.onopen do |handshake|
-      ws.send "Connected to #{handshake.path}."
-    end
-
-    ws.onclose do
-      ws.send "Closed."
-      EM.stop
-    end
-
-    ws.onmessage do |msg|
-      puts "msg: #{msg}"
-      ws.send "Received Message: #{msg}"
-    end
-  end
-
-  CarryYourWeight::Server.run!
-end
-
+# start the application
+run app: CarryYourWeight::Server.new
